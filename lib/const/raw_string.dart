271b9f53cbe814037962cc91693c8c2a6f87b348
ahsan <rafiq.ahsan2134@gmail.com>
2024-10-04 12:13:42 +0500

    new app

---
+++ lib/const/raw_string.dart
@@ -0,0 +1,8 @@
+class RawString {
+  RawString._();
+  static String appName = 'Test_App';
+  static String appDescription = 'MY Test App.';
+  static String emailId = 'rafiq.ahsan2134@gmail.com';
+  static String dummyEmail = 'testapp@gmail.com';
+  static String appLogoURL = 'https://ui-avatars.com/api/?name=Super+Store';
+}
---
+++ lib/imports.dart
@@ -0,0 +1,26 @@
+export 'dart:convert';
+
+export 'package:flutter/foundation.dart';
+export 'package:flutter/material.dart';
+export 'package:google_fonts/google_fonts.dart';
+export 'package:icons_plus/icons_plus.dart';
+export 'package:provider/provider.dart';
+export 'package:test_app/const/app_colors.dart';
+export 'package:test_app/const/raw_string.dart';
+export 'package:test_app/controller/cart_provider.dart';
+export 'package:test_app/model/cart_model.dart';
+export 'package:test_app/model/product_model.dart';
+export 'package:test_app/utils/url_launch.dart';
+export 'package:test_app/view/cart/cart.dart';
+export 'package:test_app/view/drawer/drawer_menu.dart';
+export 'package:test_app/view/home/home.dart';
+export 'package:test_app/view/init_screen/login.dart';
+export 'package:test_app/view/init_screen/register.dart';
+export 'package:test_app/widgets/app_name_widget.dart';
+export 'package:test_app/widgets/card/cart_card.dart';
+export 'package:test_app/widgets/card/product_card.dart';
+export 'package:test_app/widgets/text/text_builder.dart';
+export 'package:test_app/widgets/text_field/custom_text_field.dart';
+export 'package:url_launcher/url_launcher.dart';
+
+export 'view/init_screen/splash.dart';
